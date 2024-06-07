57cdb57ddf450e1a17527db4f4e8511c7f22e529
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-06-07 02:22:31 -0300

    chore: install tailwind typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,39 +2,39 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {
-      colors: {
-        "white": "#ffffff",
-        "black": "#000000",
+    colors: {
+      "white": "#ffffff",
+      "black": "#000000",
 
-        "backgrond": "#F8F8F8",
-        "d-backgrond": "#18171D",
+      "backgrond": "#F8F8F8",
+      "d-backgrond": "#18171D",
 
-        "yellow-700": "#C57C10",
-        "yellow-600": "#FF7A00",
-        "yellow-500": "#FFC700",
-        
-        "blue-500": "#0066FF",
-        
-        "gray-800": "#242E3C",
-        "gray-600": "#4C4E59",
-        "gray-300": "#D7D9E1",
-        "gray-200": "#E9EBF1",
-        "gray-100": "#F3F5F8",
-        
-        "d-gray-800": "#DBE3ED",
-        "d-gray-600": "#A9ADB4",
-        "d-gray-300": "#3E404F",
-        "d-gray-200": "#252628",
-        "d-gray-100": "#1D1F22",
+      "yellow-700": "#C57C10",
+      "yellow-600": "#FF7A00",
+      "yellow-500": "#FFC700",
+      
+      "blue-500": "#0066FF",
+      
+      "gray-800": "#242E3C",
+      "gray-600": "#4C4E59",
+      "gray-300": "#D7D9E1",
+      "gray-200": "#E9EBF1",
+      "gray-100": "#F3F5F8",
+      
+      "d-gray-800": "#DBE3ED",
+      "d-gray-600": "#A9ADB4",
+      "d-gray-300": "#3E404F",
+      "d-gray-200": "#252628",
+      "d-gray-100": "#1D1F22",
 
-        "violet-500": "#8F00FF",
+      "violet-500": "#8F00FF",
 
-        "green-500": "#1BB82B",
-      },
+      "green-500": "#1BB82B",
     },
   },
   darkMode: "selector",
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
 
