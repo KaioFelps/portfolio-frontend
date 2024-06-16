ffa4ec761f5d28fca01f9b2e9cf51b075764214c
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-06-15 23:37:27 -0300

    build: add logger

---
+++ src/hooks.server.ts
@@ -0,0 +1,37 @@
+import type { Handle } from "@sveltejs/kit";
+import winston from "winston";
+
+const logger = winston.createLogger({
+	level: "info",
+	format: winston.format.json(),
+	defaultMeta: { service: "user-service" },
+	transports: [
+		//
+		// - Write all logs with importance level of `error` or less to `error.log`
+		// - Write all logs with importance level of `info` or less to `combined.log`
+		//
+		new winston.transports.File({ filename: "error.log", level: "error" }),
+		new winston.transports.File({ filename: "combined.log" }),
+	],
+});
+
+//
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+logger.add(
+	new winston.transports.Console({
+		format: winston.format.simple(),
+	}),
+);
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const { url, getClientAddress } = event;
+
+	logger.info("Request recebida em " + new Date().toString(), {
+		url: url.href,
+		client: getClientAddress(),
+	});
+
+	const response = await resolve(event);
+	return response;
+};
