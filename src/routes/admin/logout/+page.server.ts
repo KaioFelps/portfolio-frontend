3887ed8d66f76e5a6e65c9e9466bfe8251834217
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-08-03 21:17:20 -0300

    fix: logout route

--- src/routes/admin/logout/+page.server.ts
+++ src/routes/admin/logout/+page.server.ts
@@ -2,10 +2,8 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = ({ cookies, locals }) => {
-	if (locals.accessToken) {
-		cookies.delete("refresh_token", { path: "/" });
-		locals.user = undefined;
-		locals.accessToken = undefined;
-		return redirect(302, "/admin/login");
-	}
+	cookies.delete("refresh_token", { path: "/" });
+	locals.user = undefined;
+	locals.accessToken = undefined;
+	return redirect(302, "/admin/login");
 };
