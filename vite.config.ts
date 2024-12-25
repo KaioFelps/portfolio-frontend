f13b32e344f23c19dc198bc97d53f4395c79e94e
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-12-24 21:10:08 -0300

    chore: add rollup visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-	plugins: [sveltekit(), tsconfigPaths()],
+	plugins: [sveltekit(), tsconfigPaths(), visualizer()],
 });
