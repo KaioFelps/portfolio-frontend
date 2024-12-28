b7ed275a6e886580fe1d671b3fe400457756c0c9
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-12-28 16:47:47 -0300

    feat: add codeblock identation with spaces and tabs

--- src/lib/tiptap/indentCodeBlock.ts
+++ src/lib/tiptap/indentCodeBlock.ts
@@ -17,6 +17,28 @@ const CodeBlockIndent = Extension.create({
 						let tabbed = false;
 						doc.nodesBetween(from, to, (node, _pos) => {
 							if (node.type.name == "codeBlock") {
+								tr.insertText("\t");
+								tabbed = true;
+							}
+						});
+
+						return tabbed;
+					})
+					.run(); // <- make sure to return true to prevent the tab from blurring.
+			},
+			"Shift-Tab": ({ editor }) => {
+				return editor
+					.chain()
+					.command(({ tr }) => {
+						const { doc, selection } = tr;
+
+						if (!doc || !selection || !(selection instanceof TextSelection)) return false;
+
+						const { from, to } = selection;
+
+						let tabbed = false;
+						doc.nodesBetween(from, to, (node, _pos) => {
+							if (node.type.name == "codeBlock") {
 								tr.insertText("  ");
 								tabbed = true;
 							}
