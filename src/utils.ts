eb46b8a8f6dbff502f2170970f883108adb722d7
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-18 14:54:48 -0300

    fix: `getYearsFromNow`

--- src/utils.ts
+++ src/utils.ts
@@ -103,8 +103,8 @@ export function getYearsFromNow(date: Date) {
 	const now = new Date();
 
 	// difference in days;
-	const diff = (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
+	const diff = (now.getTime() - date.getTime()) / 1000 / 60 / 60 / 24;
 
-	const yearsDifference = Math.abs(Math.round(diff / 365.25));
+	const yearsDifference = Math.abs(Math.floor(diff / 365.25));
 	return yearsDifference;
 }
