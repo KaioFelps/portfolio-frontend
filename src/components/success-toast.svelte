062cce0429e3c251b4f26119633f095a33ec5e6f
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-01 16:12:06 -0300

    feat: add `SuccessToast` component

---
+++ src/components/success-toast.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Check from "phosphor-svelte/lib/CheckCircle";
+	import { fly } from "svelte/transition";
+
+	type Props = {
+		message: string;
+		close: () => void;
+	};
+
+	const { message, close }: Props = $props();
+</script>
+
+<div
+	class="p-4 rounded-xl absolute right-3 bottom-3 z-20 max-w-[calc(100%_-_24px)] bg-green-500/80"
+	transition:fly
+>
+	<div class="flex items-start gap-2">
+		<Check size="20" weight="fill" class="mt-[1px]" />
+		<span>
+			{message}
+		</span>
+	</div>
+
+	<button onclick={close} class="btn ghost-dark btn-sm block ml-auto mt-1 leading-none rounded-md">
+		Fechar
+	</button>
+</div>
