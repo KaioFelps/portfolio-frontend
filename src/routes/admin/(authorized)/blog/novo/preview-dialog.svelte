06a2e5001993c2e6911bd31a6dfd94083f19e5a5
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-01 00:52:34 -0300

    fix(preview-dialog): fix dialog content child API for setting svelte transition

--- src/routes/admin/(authorized)/blog/novo/preview-dialog.svelte
+++ src/routes/admin/(authorized)/blog/novo/preview-dialog.svelte
@@ -24,31 +24,36 @@
 	<Dialog.Portal>
 		<Dialog.Overlay class="fixed inset-0 z-50 bg-black/10 backdrop-blur-sm" />
 		<Dialog.Content
-			transition={flyAndScale}
 			class="
             dark fixed left-[50%] top-[50%] z-50 translate-x-[-50%] translate-y-[-50%]
 			shadow-popover outline-none border border-white/5 rounded-2xl p-6 bg-d-backgrond/80 backdrop-blur-md
             w-main max-w-[calc(100%_-_48px)] max-h-[calc(100vh_-_72px)] overflow-y-scroll
             "
 		>
-			<header
-				class="pb-4 border-b mb-6 border-d-gray-300 text-d-gray-800 flex items-center justify-between"
-			>
-				<h1 class="font-bold text-5xl">Preview do post</h1>
+			{#snippet child({ props, open })}
+				{#if open}
+					<div {...props} transition:flyAndScale>
+						<header
+							class="pb-4 border-b mb-6 border-d-gray-300 text-d-gray-800 flex items-center justify-between"
+						>
+							<h1 class="font-bold text-5xl">Preview do post</h1>
 
-				<Dialog.Close
-					autofocus={false}
-					class="cursor-default p-3 rounded-full bg-transparent dark:bg-white/5 hover:bg-black/5
+							<Dialog.Close
+								autofocus={false}
+								class="cursor-default p-3 rounded-full bg-transparent dark:bg-white/5 hover:bg-black/5
                     dark:hover:bg-white/10 active:brightness-95 outline-none ring-0
                     dark:ring-white/15 ring-black/15 focus:ring-4 transition-all"
-				>
-					<X size="20" weight="bold" />
-				</Dialog.Close>
-			</header>
+							>
+								<X size="20" weight="bold" />
+							</Dialog.Close>
+						</header>
 
-			<div class="text-container" bind:this={textContainer}>
-				{@html html}
-			</div>
+						<div class="text-container" bind:this={textContainer}>
+							{@html html}
+						</div>
+					</div>
+				{/if}
+			{/snippet}
 		</Dialog.Content>
 	</Dialog.Portal>
 </Dialog.Root>
