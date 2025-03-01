98b028aa2df79141817284c66622007cf3483cfb
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-02-28 22:57:18 -0300

    fix(LinksPopover):  fix `Popover.Arrow` behavior and page props definition

--- src/routes/(public)/projetos/linksPopover.svelte
+++ src/routes/(public)/projetos/linksPopover.svelte
@@ -5,7 +5,7 @@
 	import clsx from "clsx";
 	import { flyAndScale } from "$crate/utils";
 
-	export let links: Link[];
+	const { links }: { links: Link[] } = $props();
 </script>
 
 {#if links.length > 0}
@@ -22,26 +22,43 @@
 		</Popover.Trigger>
 		<Popover.Content
 			sideOffset={8}
-			transition={flyAndScale}
-			class="z-30 w-full flex flex-col gap-1 max-w-48 rounded-xl border border-gray-300 dark:border-black bg-gray-100 dark:bg-d-gray-100 p-2"
+			forceMount
+			class={clsx(
+				"z-30 w-full flex flex-col gap-1 max-w-48 rounded-xl ring-1 ring-gray-300",
+				"dark:ring-black bg-gray-100 dark:bg-d-gray-100 p-2",
+			)}
 		>
-			{#each links as link}
-				<a
-					class={clsx(
-						"cursor-default block p-2 rounded-lg text-center transition-all will-change-[background-color,_filter] font-bold text-sm text-blue-500",
-						"dark:bg-d-gray-200 dark:hover:bg-white/5 dark:active:bg-white/10 dark:active:brightness-100",
-						"bg-gray-200 hover:bg-gray-300 active:brightness-90",
-					)}
-					href={link.value}
-					target="_blank"
-				>
-					{link.title}
-				</a>
-			{/each}
+			{#snippet child({ wrapperProps, props, open })}
+				{#if open}
+					<div {...wrapperProps}>
+						<div {...props} transition:flyAndScale>
+							{#each links as link}
+								<a
+									class={clsx(
+										"cursor-default block p-2 rounded-lg text-center transition-all",
+										"will-change-[background-color,_filter] font-bold text-sm text-blue-500",
+										"dark:bg-d-gray-200 dark:hover:bg-white/5 dark:active:bg-white/10",
+										"dark:active:brightness-100 bg-gray-200 hover:bg-gray-300 active:brightness-90",
+									)}
+									href={link.value}
+									target="_blank"
+								>
+									{link.title}
+								</a>
+							{/each}
 
-			<Popover.Arrow
-				class="drop-shadow-[-1px_-1px_0_var(--tw-shadow-color)] shadow-gray-300 dark:shadow-black"
-			/>
+							<Popover.Arrow
+								width={8}
+								height={4}
+								class={clsx(
+									"text-gray-100 dark:text-d-gray-200 drop-shadow-[0px_-1px_0_var(--tw-shadow-color)]",
+									"shadow-gray-300 dark:shadow-black",
+								)}
+							></Popover.Arrow>
+						</div>
+					</div>
+				{/if}
+			{/snippet}
 		</Popover.Content>
 	</Popover.Root>
 {/if}
