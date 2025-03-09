2b6c7cda7b21d134fa9398d2e324b2924180ca5a
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-09 00:43:44 -0300

    refactor(admin/blog handlers): rename load handler in order to specify which page it's loading data for

--- src/routes/admin/(authorized)/blog/novo/+page.server.ts
+++ src/routes/admin/(authorized)/blog/novo/+page.server.ts
@@ -1,8 +1,9 @@
 import type { Actions } from "@sveltejs/kit";
-import { BlogActionsHandlers } from "../handlers";
 import type { PageServerLoad } from "./$types";
+import { BlogActionsHandlers } from "../handlers";
 
-export const load: PageServerLoad = async (ctx) => await BlogActionsHandlers.load.bind(ctx)();
+export const load: PageServerLoad = async (ctx) =>
+	await BlogActionsHandlers.loadNewBlogPostPageData.bind(ctx)();
 
 export const actions: Actions = {
 	publish: async (ctx) => await BlogActionsHandlers.publish.bind(ctx)(),
