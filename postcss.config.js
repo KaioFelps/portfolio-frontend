ee2df1e2949601e673d9f98b764643778465165b
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-04-28 21:38:50 -0300

    style: fix always floating labels on firefox

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      overrideBrowserslist: [
+        "defaults and Firefox >= 51",
+      ]
+    },
   },
 }
